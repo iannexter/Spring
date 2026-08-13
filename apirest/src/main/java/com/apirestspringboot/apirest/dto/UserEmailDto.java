f1955c885d00da64f022f8c73b85b84c272b26f6
Ian Nicolas Notebook <iansistemasbr@gmail.com>
2026-08-13 03:25:05 -0300

    Controller

---
+++ apirest/src/main/java/com/apirestspringboot/apirest/dto/UserEmailDto.java
@@ -0,0 +1,20 @@
+package com.apirestspringboot.apirest.dto;
+
+
+import jakarta.validation.constraints.NotBlank;
+
+public class UserEmailDto {
+
+    @NotBlank
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+}
