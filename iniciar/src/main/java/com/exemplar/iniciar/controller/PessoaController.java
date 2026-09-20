bffe5bd5bcffe7280e133a6fae737bfe812d8167
Ian Nicolas <iansistemasbr@gmail.com>
2026-09-20 14:32:48 -0300

    Tornando mais legivel o PessoaController e o formulario.html

--- iniciar/src/main/java/com/exemplar/iniciar/controller/PessoaController.java
+++ iniciar/src/main/java/com/exemplar/iniciar/controller/PessoaController.java
@@ -53,26 +53,106 @@ public class PessoaController {
     }
 
 
-        @GetMapping("/nova")
-        public String mostrarFormularioDeNovaPessoa(Model model){
 
-        model.addAttribute("pessoa", new Pessoa());
 
-        model.addAttribute("acao", "/pessoas/nova");
+    @GetMapping("/nova")
+    public String mostrarFormulario(Model model) {
+
+        Pessoa pessoa = new Pessoa();
+
+        model.addAttribute("pessoa", pessoa);
 
         return "formulario";
+    }
 
-        }
+
+    @PostMapping("/nova")
+    public String salvarPessoa(@ModelAttribute Pessoa pessoa) {
+
+        pessoaService.criarPessoa(pessoa);
+
+        return "redirect:/pessoas";
+    }
 
 
-        @PostMapping("/nova")
-        public String salvarNovaPessoa(@ModelAttribute Pessoa pessoa){
 
-            pessoaService.criarPessoa(pessoa);
 
-            return "redirect:/pessoas";
 
-        }
+
+
+
+
+
+
+//        @GetMapping("/nova")
+//        public String mostrarFormularioDeNovaPessoa(Model model){
+//
+//        model.addAttribute("pessoa", new Pessoa());
+
+
+        //model.addAttribute("acao", "/pessoas/nova");
+        //
+        //Então o Thymeleaf gera algo equivalente a:
+        //
+        //<form action="/pessoas/nova" method="post">
+
+        //"Coloque dentro do Model uma informação chamada acao,
+        // cujo valor é /pessoas/nova."
+        //
+        //Imagine o Model como uma caixa
+        // que o Controller entrega para o HTML.
+        //O "acao" é o nome que o Thymeleaf vai usar para
+        // encontrar essa informação.
+        //
+        //E "/pessoas/nova" é o valor.
+
+            //coloca esse objeto dentro do Model com o nome "pessoa":
+            //
+            //Model
+            //┌────────────────────────────┐
+            //│ "pessoa" → Pessoa          │
+            //│                            │
+            //│            ┌────────────┐  │
+            //│            │ id = null  │  │
+            //│            │ nome=null  │  │
+            //│            │ idade=0    │  │
+            //│            └────────────┘  │
+            //└────────────────────────────┘
+            //2. O th:object="${pessoa}"
+            //
+            //Agora o Thymeleaf recebe esse Model.
+            //
+            //Você escreveu:
+            //
+            //<form th:object="${pessoa}">
+            //
+            //O:
+            //
+            //${pessoa}
+            //
+            //significa:
+            //
+            //"Pegue do Model o objeto chamado pessoa."
+            //
+            //Então:
+            //
+            //th:object="${pessoa}"
+//        model.addAttribute("acao", "/pessoas/nova");
+//
+//        return "formulario";
+//
+//        }
+
+        //Ele vai receber do front-end via POST um objeto
+        //@ModelAttribute
+//        @PostMapping("/nova")
+//        public String salvarNovaPessoa(@ModelAttribute Pessoa pessoa){
+//
+//            pessoaService.criarPessoa(pessoa);
+//
+//            return "redirect:/pessoas";
+//
+//        }
 
 
 
